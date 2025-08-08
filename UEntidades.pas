358fd3a1f74158332caf1f96eb56284affd75e6c
Gabrielle Clemente <gabiclemente2007@gmail.com>
2025-08-08 17:05:42 -0300

    Update UEntidades.pas (Começando POO do zero)

--- UEntidades.pas
+++ UEntidades.pas
@@ -2,49 +2,23 @@ unit UEntidades;
 
 interface
 
-uses
-System.Classes,  System.Generics.Collections, Dialogs, Vcl.Grids;
-
-    type
-     TEntidades = class (TObject)
-     public
-     procedure IncluirDados(Grid: TStringGrid; Codigo, Nome: string);
-     procedure ListarDados;
-     procedure AtualizarDados;
-     procedure ExcluirDados;
-
-    end;
+type
+  TEntidades = class
+  private
+    FCodigo: Integer;
+    FNome: string;
+  public
+    constructor Create(Codigo: Integer; Nome: string);
+    property Codigo: Integer read FCodigo write FCodigo;
+    property Nome: string read FNome write FNome;
+  end;
 
 implementation
- uses UMain2;
 
-{ TEntidades }
-
-procedure TEntidades.AtualizarDados;
+constructor TEntidades.Create(Codigo: Integer; Nome: string);
 begin
-
-end;
-
-procedure TEntidades.ExcluirDados;
-begin
-   ShowMessage ('Dados excuidos');
-end;
-
-procedure TEntidades.IncluirDados(Grid: TStringGrid; Codigo, Nome: string);
-
-var
-  NovaLinha: Integer;
-begin
-  NovaLinha := Grid.RowCount;
-  Grid.RowCount := NovaLinha + 1;
-
-  Grid.Cells[0, NovaLinha] := Codigo;
-  Grid.Cells[1, NovaLinha] := Nome;
-end;
-
-procedure TEntidades.ListarDados;
-begin
-
+  FCodigo := Codigo;
+  FNome := Nome;
 end;
 
 end.
