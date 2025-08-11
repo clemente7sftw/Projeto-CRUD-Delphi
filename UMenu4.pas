880b991de5f8732df4fd34addfda5aa11638a85a
Gabrielle Clemente <gabiclemente2007@gmail.com>
2025-08-11 17:34:59 -0300

    Update UMenu4.pas (Implementação POO)

--- UMenu4.pas
+++ UMenu4.pas
@@ -4,7 +4,7 @@ interface
 
 uses
   Winapi.Windows, Winapi.Messages, System.SysUtils, System.Variants, System.Classes, Vcl.Graphics,
-  Vcl.Controls, Vcl.Forms, Vcl.Dialogs, Vcl.Imaging.jpeg, Vcl.ExtCtrls,
+  Vcl.Controls, Vcl.Forms,UProfessores, Vcl.Dialogs, Vcl.Imaging.jpeg, Vcl.ExtCtrls,
   Vcl.Grids, Vcl.StdCtrls;
 
 type
@@ -18,11 +18,20 @@ type
     PSair: TPanel;
     PAtualizar: TPanel;
     MsgEnt: TLabel;
+    BtnConf: TButton;
+    BtnAdd: TButton;
+    Dados: TListBox;
     procedure PSairClick(Sender: TObject);
     procedure ListaKeyPress(Sender: TObject; var Key: Char);
     procedure ListaEnter(Sender: TObject);
+    procedure PIncluirClick(Sender: TObject);
+    procedure FormCreate(Sender: TObject);
+    procedure ListaKeyDown(Sender: TObject; var Key: Word; Shift: TShiftState);
+    procedure PListarClick(Sender: TObject);
+    procedure BtnConfClick(Sender: TObject);
+    procedure PExcluirClick(Sender: TObject);
   private
-    { Private declarations }
+   var Professores:TProfessor;
   public
     { Public declarations }
   end;
@@ -36,11 +45,45 @@ implementation
 
 uses UMain2;
 
+procedure TForm4.BtnConfClick(Sender: TObject);
+begin
+  Professores.ConfirmarDados;
+  Professores.Salvar;
+end;
+
+procedure TForm4.FormCreate(Sender: TObject);
+begin
+Professores := TProfessor.Create(Lista, Dados, BtnAdd, BtnConf);
+end;
+
 procedure TForm4.ListaEnter(Sender: TObject);
 begin
-    Lista.Cells[0,0]:= 'Código';
+    Lista.Cells[0,0]:= 'CÃ³digo';
     Lista.Cells[1,0]:= 'Nome';
     Lista.Cells[2,0]:= 'CPF';
+end;
+
+procedure TForm4.ListaKeyDown(Sender: TObject; var Key: Word;
+  Shift: TShiftState);
+begin
+  if Key = VK_RETURN then
+  begin
+    Key := 0;
+
+    if Lista.Row < Lista.RowCount - 1 then
+      Lista.Row := Lista.Row + 1
+    else
+    begin
+      Lista.RowCount := Lista.RowCount + 1;
+      Lista.Row := Lista.RowCount - 1;
+      Lista.Cells[0, Lista.Row] := IntToStr(100 + Lista.Row);
+      Lista.Cells[1, Lista.Row] := '';
+      Lista.Cells[2, Lista.Row] := '';
+    end;
+
+    Lista.Col := 1;
+    Lista.SetFocus;
+  end;
 end;
 
 procedure TForm4.ListaKeyPress(Sender: TObject; var Key: Char);
@@ -51,6 +94,21 @@ begin
     Lista.Row := Lista.RowCount - 1;
     Lista.Col := 0;
   end;
+end;
+
+procedure TForm4.PExcluirClick(Sender: TObject);
+begin
+Professores.ExcluirLinha;
+end;
+
+procedure TForm4.PIncluirClick(Sender: TObject);
+begin
+Professores.MostrarIncluir;
+end;
+
+procedure TForm4.PListarClick(Sender: TObject);
+begin
+Professores.MostrarListar;
 end;
 
 procedure TForm4.PSairClick(Sender: TObject);
