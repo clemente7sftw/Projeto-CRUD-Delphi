20f791b2bfe20fbe4b95f889d4fe6093afb1ee90
Gabrielle Clemente <gabiclemente2007@gmail.com>
2025-08-11 17:34:24 -0300

    Update UMenu5.pas (Implementação POO)

--- UMenu5.pas
+++ UMenu5.pas
@@ -4,7 +4,7 @@ interface
 
 uses
   Winapi.Windows, Winapi.Messages, System.SysUtils, System.Variants, System.Classes, Vcl.Graphics,
-  Vcl.Controls, Vcl.Forms, Vcl.Dialogs, Vcl.StdCtrls, Vcl.ExtCtrls, Vcl.Grids,
+  Vcl.Controls,UDisciplinas, Vcl.Forms, Vcl.Dialogs, Vcl.StdCtrls, Vcl.ExtCtrls, Vcl.Grids,
   Vcl.Imaging.jpeg;
 
 type
@@ -18,10 +18,20 @@ type
     PSair: TPanel;
     PAtualizar: TPanel;
     MsgEnt: TLabel;
+    BtnAdd: TButton;
+    BtnConf: TButton;
+    Dados: TListBox;
     procedure PSairClick(Sender: TObject);
     procedure ListaEnter(Sender: TObject);
+    procedure PIncluirClick(Sender: TObject);
+    procedure BtnConfClick(Sender: TObject);
+    procedure PExcluirClick(Sender: TObject);
+    procedure FormCreate(Sender: TObject);
+    procedure PListarClick(Sender: TObject);
+    procedure ListaKeyDown(Sender: TObject; var Key: Word; Shift: TShiftState);
+    procedure ListaKeyPress(Sender: TObject; var Key: Char);
   private
-    { Private declarations }
+   var Disciplinas:TDisciplina;
   public
     { Public declarations }
   end;
@@ -35,10 +45,69 @@ implementation
 
 uses UMain2;
 
+
+procedure TForm5.BtnConfClick(Sender: TObject);
+begin
+  Disciplinas.ConfirmarDados;
+  Disciplinas.Salvar;
+end;
+
+procedure TForm5.FormCreate(Sender: TObject);
+begin
+Disciplinas := TDisciplina.Create(Lista, Dados, BtnAdd, BtnConf);
+end;
+
 procedure TForm5.ListaEnter(Sender: TObject);
 begin
-    Lista.Cells[0,0]:= 'Código';
+    Lista.Cells[0,0]:= 'CÃ³digo';
     Lista.Cells[1,0]:= 'Disciplina';
+end;
+
+procedure TForm5.ListaKeyDown(Sender: TObject; var Key: Word;
+  Shift: TShiftState);
+begin
+  if Key = VK_RETURN then
+  begin
+    Key := 0;
+
+    if Lista.Row < Lista.RowCount - 1 then
+      Lista.Row := Lista.Row + 1
+    else
+    begin
+      Lista.RowCount := Lista.RowCount + 1;
+      Lista.Row := Lista.RowCount - 1;
+      Lista.Cells[0, Lista.Row] := IntToStr(10+ Lista.Row);
+      Lista.Cells[1, Lista.Row] := '';
+      Lista.Cells[2, Lista.Row] := '';
+    end;
+
+    Lista.Col := 1;
+    Lista.SetFocus;
+  end;
+end;
+procedure TForm5.ListaKeyPress(Sender: TObject; var Key: Char);
+begin
+  if Key = #13 then
+  begin
+    Lista.RowCount := Lista.RowCount + 1;
+    Lista.Row := Lista.RowCount - 1;
+    Lista.Col := 0;
+  end;
+end;
+
+procedure TForm5.PExcluirClick(Sender: TObject);
+begin
+Disciplinas.ExcluirLinha;
+end;
+
+procedure TForm5.PIncluirClick(Sender: TObject);
+begin
+  Disciplinas.MostrarIncluir;
+end;
+
+procedure TForm5.PListarClick(Sender: TObject);
+begin
+Disciplinas.MostrarListar;
 end;
 
 procedure TForm5.PSairClick(Sender: TObject);
@@ -46,4 +115,4 @@ begin
     Form2.Show;
 end;
 
-end.
+end.0
